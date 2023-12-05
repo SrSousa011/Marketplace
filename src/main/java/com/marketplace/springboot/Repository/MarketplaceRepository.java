802bf831950e0ea2275667ac7f6ec28a2fd7bd44
Lucas Sousa <seu@email.com>
2023-12-05 17:22:23 +0100

    [ADD] Marketplace Repository

--- src/main/java/com/marketplace/springboot/Repository/MarketplaceRepository.java
+++ src/main/java/com/marketplace/springboot/Repository/MarketplaceRepository.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.marketplace.springboot.Model.MarketplaceModel;
+
 
 @Repository
 public interface MarketplaceRepository extends JpaRepository<MarketplaceModel, UUID> {
