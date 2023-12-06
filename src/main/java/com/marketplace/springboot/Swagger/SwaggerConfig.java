2efdb7c736390cb301e9ec4f874511d5aaae465b
Lucas Sousa <seu@email.com>
2023-12-06 12:55:30 +0100

    [ADD] Add Swagger documentation

---
+++ src/main/java/com/marketplace/springboot/Swagger/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.marketplace.springboot.Swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@SpringBootApplication
+@OpenAPIDefinition
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.marketplace.springboot.Controller"))
+                .paths(PathSelectors.regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        return new ApiInfoBuilder()
+                .title("Marketplace API REST")
+                .description("API REST for managing marketplace products.")
+                .version("1.0")
+                .contact(new Contact("Lucas Sousa", "https://github.com/SrSousa011", "lucas.sousa_99@hotmail.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .build();
+    }
+}
