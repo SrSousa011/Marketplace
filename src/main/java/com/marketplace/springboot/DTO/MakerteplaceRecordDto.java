43b946743dc0b706e0362484fdca8b43d0500b15
Lucas Sousa <seu@email.com>
2023-12-06 08:39:39 +0100

    [UPDATE] Chane the variable value to price

--- src/main/java/com/marketplace/springboot/DTO/MakerteplaceRecordDto.java
+++ src/main/java/com/marketplace/springboot/DTO/MakerteplaceRecordDto.java
@@ -5,6 +5,6 @@ import java.math.BigDecimal;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record MakerteplaceRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+public record MakerteplaceRecordDto(@NotBlank String name, @NotNull BigDecimal price) {
     
 }
