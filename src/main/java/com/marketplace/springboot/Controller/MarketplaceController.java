bab478331e89aec209abcf5bd102696eef2e65c3
Lucas Sousa <seu@email.com>
2023-12-06 13:12:03 +0100

    [ADD] Add Swagger documentation

--- src/main/java/com/marketplace/springboot/Controller/MarketplaceController.java
+++ src/main/java/com/marketplace/springboot/Controller/MarketplaceController.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,16 +24,16 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping("/api")
-@ApiOperation(value = "Marketplace API REST")
-@CrossOrigin(origins = "*")
+@RequestMapping("/")
+@Tag(name = "Marketplace API REST", description = "Endpoints for managing marketplace products.")
+
 public class MarketplaceController {
 
     @Autowired
     MarketplaceRepository marketplaceRepository;
 
     @GetMapping("/products")
-    @ApiOperation("Retrieve a list of all products available in the marketplace.")
+    @Operation(summary = "Retrieve a list of all products")
     public ResponseEntity<List<MarketplaceModel>> getAllProducts() {
         List<MarketplaceModel> productsList = marketplaceRepository.findAll();
         if (!productsList.isEmpty()) {
@@ -44,7 +46,7 @@ public class MarketplaceController {
     }
 
     @GetMapping("/product/{id}")
-    @ApiOperation("Retrieve details of a specific product by its ID.")
+    @Operation(summary = "Retrieve details of a specific product by its ID.")
     public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id) {
         Optional<MarketplaceModel> productO = marketplaceRepository.findById(id);
         if (productO.isEmpty()) {
@@ -55,7 +57,7 @@ public class MarketplaceController {
     }
 
     @PostMapping("/products")
-    @ApiOperation("Save a new product with the provided details.")
+    @Operation(summary = "Save a new product with the provided details.")
     public ResponseEntity<MarketplaceModel> saveProduct(
             @RequestBody @Valid MakerteplaceRecordDto makerteplaceRecordDto) {
         var marketplaceModel = new MarketplaceModel();
@@ -64,7 +66,7 @@ public class MarketplaceController {
     }
 
     @DeleteMapping("/product/{id}")
-    @ApiOperation("Delete a product with the specified ID.")
+    @Operation(summary = "Delete a product with the specified ID.")
     public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id) {
         Optional<MarketplaceModel> productO = marketplaceRepository.findById(id);
         if (productO.isEmpty()) {
@@ -75,7 +77,7 @@ public class MarketplaceController {
     }
 
     @PutMapping("/products/{id}")
-    @ApiOperation("Update details of a product with the specified ID.")
+    @Operation(summary = "Update details of a product with the specified ID.")
     public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
                                                 @RequestBody @Valid MakerteplaceRecordDto makerteplaceRecordDto) {
         Optional<MarketplaceModel> productO = marketplaceRepository.findById(id);
@@ -87,3 +89,4 @@ public class MarketplaceController {
         return ResponseEntity.status(HttpStatus.OK).body(marketplaceRepository.save(marketplaceModel));
     }
 }
+
--- src/main/java/com/marketplace/springboot/Swagger/SwaggerConfig.java
+++ src/main/java/com/marketplace/springboot/Swagger/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.marketplace.springboot.Swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,18 +14,17 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@SpringBootApplication
-@OpenAPIDefinition
+@OpenAPIDefinition(info = @Info(title = "Marketplace API REST", version = "1.0"))
 public class SwaggerConfig {
+
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.marketplace.springboot.Controller"))
-                .paths(PathSelectors.regex("/api.*"))
-                .build()
-                .apiInfo(metaInfo());
+                .apis(RequestHandlerSelectors.basePackage("com.marketplace.springboot"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
     private ApiInfo metaInfo() {
